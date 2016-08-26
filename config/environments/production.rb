55365c0349c477a21ae447c2d030ef7a743c691f
tchungnz <tchung.nz@gmail.com>
2016-08-26 11:47:33 +0100

    Updated AWS region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,7 @@ Rails.application.configure do
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :region => ENV['AWS_REGION']
   }
 }
 
