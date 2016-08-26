2ea5e6b6a0af082522ae76dc9fd85a6cb7594494
tchungnz <tchung.nz@gmail.com>
2016-08-26 14:32:59 +0100

    updated title

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,11 @@ Rails.application.configure do
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
+      url: ':s3_domain_url',
+      path: '/:class/:attachment/:id_partition/:style/:filename',
+      s3_host_name: 's3-eu-west-1.amazonaws.com'
     }
+
   }
 
 end
